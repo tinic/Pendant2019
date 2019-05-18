7250e5d1f9a19cca8cce8733f3a2ba7c0b1fe537
Tinic Uro <tinicuro@gmail.com>
2019-05-18 03:43:25 -0700

    Update compiler.

--- Pendant2019/Pendant2019/model.cpp
+++ Pendant2019/Pendant2019/model.cpp
@@ -22,7 +22,7 @@ std::string Model::CurrentBatteryVoltageString() {
 	char str[8];
 	int32_t i = int32_t(CurrentBatteryVoltage());
 	int32_t f = int32_t(fmodf(CurrentBatteryVoltage(),1.0f)*100);
-	sprintf(str,"%1d.%02d", i, f);
+	sprintf(str,"%1d.%02d", int(i), int(f));
 	return std::string(str);
 }
 
@@ -30,7 +30,7 @@ std::string Model::CurrentSystemVoltageString() {
 	char str[8];
 	int32_t i = int32_t(CurrentSystemVoltage());
 	int32_t f = int32_t(fmodf(CurrentSystemVoltage(),1.0f)*100);
-	sprintf(str,"%1d.%02d", i, f);
+	sprintf(str,"%1d.%02d", int(i), int(f));
 	return std::string(str);
 }
 
@@ -38,13 +38,13 @@ std::string Model::CurrentVbusVoltageString() {
 	char str[8];
 	int32_t i = int32_t(CurrentVbusVoltage());
 	int32_t f = int32_t(fmodf(CurrentVbusVoltage(),1.0f)*100);
-	sprintf(str,"%1d.%02d", i, f);
+	sprintf(str,"%1d.%02d", int(i), int(f));
 	return std::string(str);
 }
 
 std::string Model::CurrentChargeCurrentString() {
 	char str[8];
 	int32_t i = int32_t(CurrentChargeCurrent());
-	sprintf(str,"%d", i);
+	sprintf(str,"%d", int(i));
 	return std::string(str);
 }
